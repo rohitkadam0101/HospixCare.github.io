f450747b3fc128a547c1f1a6294837f3a345e26c
rohitkadam0101 <63335735+rohitkadam0101@users.noreply.github.com>
2020-05-28 15:02:22 +0530

    Publishing my hospital website

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,10 +1,23 @@
 //For Navbar Scroll
+var preloader = document.getElementById("loading");
+function myFunction(){
+    preloader.style.display= "none";
+}
+preloader.display = "none"
 $(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() >700);
+	$('nav').toggleClass('scrolled', $(this).scrollTop() >100);
 });
 
 //For counter
-$('.counter').counterUp({
+$(".counter").counterUp({
     delay: 10,
-    time: 2000
+    time: 1000,
 });
+
+//for carousel
+
+$('.carousel').carousel({
+  interval: 4000,
+})
+
+//for loader
